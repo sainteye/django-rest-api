9c60c74673edb9c2a8c1658743511b4f365ca0dd
sainteye <sainteye.x.deatheye@gmail.com>
2017-10-06 00:17:12 +0800

    update version

--- rest_api/__init__.py
+++ rest_api/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-__version__ = (0, 8, 2)
+__version__ = (0, 8, 3)
 __author__ = "Sean Cheng"
 __contact__ = "sean.cheng@tsunamiworks.com"
 __homepage__ = "https://github.com/sainteye/django-rest-api"
